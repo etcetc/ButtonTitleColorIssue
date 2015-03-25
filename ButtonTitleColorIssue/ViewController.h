0fdd4ead7fece0f7e295068b8aa5094aa398f58f
etcetc <ff@onebeat.com>
2015-03-25 09:28:15 -0700

    Initial Commit

---
+++ ButtonTitleColorIssue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ButtonTitleColorIssue
+//
+//  Created by Farhad Farzaneh on 3/25/15.
+//  Copyright (c) 2015 Onebeat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
